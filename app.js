9bb54eccdd9ad4942d9f67492fa9555d8bca4439
AsZaychik <aszaychik@gmail.com>
2022-12-12 00:41:15 +0700

    VUEJS

--- app.js
+++ app.js
@@ -1,40 +1,98 @@
-
 const app = new Vue({
   el: "#app",
   data: {
-    maximum: 2,
     show: false,
     topics: [
       {
         "id": 1,
         "title": "topic 1",
-        "sub": "subtopic 1",
-        "body":
-          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?",
+        "sub": [
+          {
+            "title": "subtopic 1.1",
+            "body": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?"
+          },
+          {
+            "title": "subtopic 1.2",
+            "body": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?"
+          },
+          {
+            "title": "subtopic 1.3",
+            "body": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?"
+          },
+          {
+            "title": "subtopic 1.4",
+            "body": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?"
+          },
+          {
+            "title": "subtopic 1.5",
+            "body": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?"
+          },
+          {
+            "title": "subtopic 1.6",
+            "body": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?"
+          },
+        ],
       },
       {
         "id": 2,
         "title": "topic 2",
-        "sub": "subtopic 2",
-        "body":
-          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?",
+        "sub": [
+          {
+            "title": "subtopic 2.1",
+            "body": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?"
+          },
+          {
+            "title": "subtopic 2.2",
+            "body": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?"
+          },
+          {
+            "title": "subtopic 2.3",
+            "body": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?"
+          },
+        ],
       },
       {
         "id": 3,
         "title": "topic 3",
-        "sub": "subtopic 3",
-        "body":
-          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?",
+        "sub": [
+          {
+            "title": "subtopic 3.1",
+            "body": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?"
+          },
+          {
+            "title": "subtopic 3.2",
+            "body": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?"
+          },
+          {
+            "title": "subtopic 3.3",
+            "body": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?"
+          },
+          {
+            "title": "subtopic 3.4",
+            "body": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?"
+          },
+          {
+            "title": "subtopic 3.5",
+            "body": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat qui laudantium quia neque magnam eveniet? Quia dolores optio ipsam distinctio veniam, in minima cum quisquam, voluptates exercitationem, inventore a aut?"
+          },
+        ],
       },
     ],
   },
   methods: {
-    showItem: function (show) {
-
-      if (show == true) {
-        this.maximum = 3
-        console.log(this.maximum);
+    isEven: function (id) {
+      this.id = id;
+      if (this.id % 2 !== 0) {
+        return false;
       }
+      return true;
+    },
+    isOdd: function (id) {
+      this.id = id;
+      if (this.id % 2 !== 1) {
+        return false;
+      }
+      return true;
     }
   },
 });
